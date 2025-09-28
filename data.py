590243eec03412817a9e11d6ba024bb0f6854bb3
keonphan <keonleon794@gmail.com>
2025-09-27 21:10:49 -0400

    added data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+def calculate_attention_score(desired_attention, actual_time_on_component, total_test_time):
+
+    margin_error = 5
+
+    actual_attention = (actual_time_on_component / total_test_time) * 100
+    attention_gap = actual_attention - desired_attention
+
+    # Determine if the actual attention is within the margin of error
+    within = abs(attention_gap) <= margin_error
+
+    return {
+        "score": attention_gap,
+        "within": within,
+        "desiredAttention": desired_attention,
+        "actualAttention": round(actual_attention, 2),
+    }
